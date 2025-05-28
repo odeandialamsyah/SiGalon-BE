3e7bd500daebe19eae72cca2cc736708e03989d6
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-28 23:02:13 +0700

    iniasialisasi collection from  db

--- repositories/userRepositories.go
+++ repositories/userRepositories.go
@@ -1,6 +1,13 @@
 package repositories
 
+import ()
+
 var (
 	userCollection *mongo.Collection
 	roleCollection *mongo.Collection
 )
+
+func InitCollections() {
+	userCollection = config.DB.Collection("users")
+	roleCollection = config.DB.Collection("roles")
+}
