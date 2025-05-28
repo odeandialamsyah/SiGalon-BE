fb91fb0466965362be416b317d244d6625397fc8
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-28 23:11:01 +0700

    func for get user by username

--- repositories/userRepositories.go
+++ repositories/userRepositories.go
@@ -1,6 +1,13 @@
 package repositories
 
-import ()
+import (
+	"context"
+	"sigalon-be/config"
+	"sigalon-be/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var (
 	userCollection *mongo.Collection
@@ -10,4 +17,14 @@ var (
 func InitCollections() {
 	userCollection = config.DB.Collection("users")
 	roleCollection = config.DB.Collection("roles")
+}
+
+// Fungsi untuk mendapatkan user berdasarkan username
+func GetUserByUsername(username string) (models.User, error) {
+	var user models.User
+	err := userCollection.FindOne(context.Background(), bson.M{"username": username}).Decode(&user)
+	if err != nil {
+		return models.User{}, err
+	}
+
 }
