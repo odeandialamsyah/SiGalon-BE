aa9352d0390d849121cc06694d13db0b14c282bb
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-28 23:11:52 +0700

    get role information to completed func get user

--- repositories/userRepositories.go
+++ repositories/userRepositories.go
@@ -27,4 +27,13 @@ func GetUserByUsername(username string) (models.User, error) {
 		return models.User{}, err
 	}
 
+	// Get role information
+	var role models.Role
+	err = roleCollection.FindOne(context.Background(), bson.M{"_id": user.RoleID}).Decode(&role)
+	if err != nil {
+		return models.User{}, err
+	}
+
+	user.Role = role
+	return user, nil
 }
