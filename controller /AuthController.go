e7fd686a2ad3c31b88d4086d7074ba9a58c0be5d
ODE ANDI ALAMSYAH <147012411+odeandialamsyah@users.noreply.github.com>
2025-06-05 23:50:58 +0700

    var logindata

--- controller /AuthController.go
+++ controller /AuthController.go
@@ -2,5 +2,9 @@ package controller
 
 // Fungsi login user
 func LoginUser(c *fiber.Ctx) error {
-	
+	var loginData struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
 }
